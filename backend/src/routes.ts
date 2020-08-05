fc9ec5ee2fdbb24828cb8dd160cdd2730e2e140d
brenosekiro <b88660501@gmail.com>
2020-08-04 23:19:38 -0300

    relations

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express'
-
+import ClassesController from './controllers/ClassesControllers'
 const routes = Router()
+
+
+routes.post('/classes', ClassesController.create)
+routes.get('/classes', ClassesController.index)
 
 export default routes

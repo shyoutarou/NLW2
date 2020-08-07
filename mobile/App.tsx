8eedf4c22cf9f275f47a74846bd76ae07b4526bc
brenosekiro <b88660501@gmail.com>
2020-08-06 22:01:02 -0300

    landingPage

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Landing from './src/Pages/Landing'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hi!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Landing />
+      <StatusBar style='light' />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/src/Pages/Landing/index.tsx
@@ -0,0 +1,108 @@
+import React from 'react'
+import { View, StyleSheet, Image, Text, TouchableOpacity } from 'react-native'
+import { AppLoading } from 'expo'
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo'
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
+import { RectButton } from 'react-native-gesture-handler'
+
+const Landing = () => {
+
+    let [fontsLoaded] = useFonts({
+        Archivo_400Regular,
+        Archivo_700Bold,
+        Poppins_400Regular,
+        Poppins_600SemiBold
+    })
+
+    if(!fontsLoaded) {
+        return <AppLoading />
+    }
+
+    return (
+        <View style={styles.container}>
+            <Image style={styles.banner} source={require('../../../assets/images/landing.png')} />
+            <Text style={styles.title}>
+                Seja bem vindo, {'\n'}
+                <Text style={styles.titleBold}>O que deseja fazer?</Text>
+            </Text>
+
+            <View style={styles.buttonsContainer}>
+                <RectButton style={[styles.button, styles.buttonPrimary]}>
+                    <Image source={require('../../../assets/images/icons/study.png')} />
+                    <Text style={styles.buttonText}>
+                        Estudar
+                    </Text>
+                </RectButton>
+
+                <RectButton style={[styles.button, styles.buttonSecondary]}>
+                    <Image source={require('../../../assets/images/icons/give-classes.png')} />
+                    <Text style={styles.buttonText}>
+                        Dar aulas
+                    </Text>
+                </RectButton>
+            </View>
+
+            <Text style={styles.totalConnections}>
+                Total de 250 conexões já realizdas {' '}
+                <Image source={require('../../../assets/images/icons/heart.png')} />
+            </Text>
+        </View>
+    )
+}
+
+export default Landing
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#8257e5",
+        flex: 1,
+        justifyContent: 'center',
+        padding: 40
+    },
+    banner: {
+        width: '100%',
+        resizeMode: 'contain'
+    },
+    title: {
+        fontSize: 20,
+        color: '#fff',
+        lineHeight: 30,
+        marginTop: 80,
+        fontFamily: 'Poppins_400Regular'
+    },
+    titleBold: {
+        fontFamily: 'Poppins_600SemiBold'
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        marginTop: 40,
+        justifyContent: 'space-between'
+    },
+    button: {
+        height: 150,
+        width: '48%',
+        backgroundColor: '#333',
+        borderRadius: 8,
+        padding: 24,
+        justifyContent: 'space-between'
+    },
+    buttonPrimary: {
+        backgroundColor: '#9871f5'
+    },
+    buttonSecondary: {
+        backgroundColor: '#04d361'
+    },
+    buttonText: {
+        fontFamily: 'Archivo_700Bold',
+        color: 'white',
+        fontSize: 20
+    },
+    totalConnections: {
+        fontFamily: 'Poppins_400Regular',
+        color: '#d4c2ff',
+        fontSize: 12,
+        lineHeight: 20,
+        maxWidth: 140,
+        marginTop: 40
+    }
+})

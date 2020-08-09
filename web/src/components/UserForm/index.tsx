b733e0e8022e5e27dc46442f2d7353c4ec6c6d37
brenosekiro <b88660501@gmail.com>
2020-08-08 23:55:14 -0300

    form-structure

--- web/src/components/UserForm/index.tsx
+++ web/src/components/UserForm/index.tsx
@@ -11,11 +11,27 @@ const UserForm = () => {
                 <div className="userform-logo">
                     <img className='userform-logo-details' src={details} alt=""/>
                     <img className='userform-logo-details' src={details2} alt=""/>
-                    <img width='300' className='userform-logo-proffy' src={logoImage} alt=""/>
+                    <img className='userform-logo-proffy' src={logoImage} alt=""/>
                 </div>
             </div>
             <div className="userform-form">
-
+                <form>
+                    <h3>Fazer login</h3>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <div>
+                        <div>
+                            <input type="checkbox"/>
+                            <p>Lembrar-me</p>
+                        </div>
+                        <p>Esqueci minha senha</p>
+                    </div>
+                    <button>Entrar</button>
+                </form>
+                <div>
+                    <p>Não tem conta? <strong>Cadastre-se</strong></p>
+                    <p>É de graça s2</p>
+                </div>
             </div>
         </div>
     )

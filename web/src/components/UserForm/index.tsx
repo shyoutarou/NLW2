46011adfe753c16cd968acf66bb6a55ed1741d8b
brenosekiro <b88660501@gmail.com>
2020-08-09 02:31:55 -0300

    userFormCompleted

--- web/src/components/UserForm/index.tsx
+++ web/src/components/UserForm/index.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logoImage from '../../assets/images/logo.svg'
 import details from '../../assets/images/details.svg'
 import details2 from '../../assets/images/details2.svg'
-import { FiCheck, FiHeart } from 'react-icons/fi'
+import { FiCheck, FiEye, FiEyeOff } from 'react-icons/fi'
 import { FaHeart } from 'react-icons/fa'
 import './styles.css'
 
 const UserForm = () => {
+
+    const [remember, setRemember] = useState(false)
+    const [visible, setVisible] = useState(false)
+
     return (
         <div className="userform-container">
             <div className="userform-banner">
@@ -20,14 +24,29 @@ const UserForm = () => {
                 <form>
                     <h3 className="userform-title">Fazer login</h3>
                     <div className="userform-inputs">
-                        <input placeholder='Digite seu e-mail' className="userform-email" type="text"/>
-                        <input placeholder='Digite sua senha' className="userform-password" type="text"/>
+                        <div className="form-input">
+                            <input placeholder='Digite seu e-mail' className="userform-email" type="text" />
+                            <div className="input-border"></div>
+                        </div>
+                        <div className="form-input">
+                            <input placeholder='Digite sua senha' className="userform-password"
+                            type={visible ? "text" : "password"}/>
+                            <div className="input-border"></div>
+                            <div onClick={() => {setVisible(!visible)}} className="eye">
+                                {visible ? <FiEye color="#8257e5" size={20} /> :
+                                <FiEyeOff color="#8257e5" size={20} />}
+                            </div>
+                        </div>
                     </div>
                     <div className="options">
                         <div className="remember">
-                            <div className="checkbox">
-                                <FiCheck size={16} color='white' />
-                            </div>
+                            <label className="checkbox-container">
+                                <input onChange={e => setRemember(e.target.checked)} checked={remember}
+                                type="checkbox"/>
+                                <div className="checkbox">
+                                    <FiCheck size={16} color='white' />
+                                </div>
+                            </label>
                             <p>Lembrar-me</p>
                         </div>
                         <p className="forget-password">Esqueci minha senha</p>

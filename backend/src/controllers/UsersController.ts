170db559fd22949acac02771eb30c07092467766
brenosekiro <b88660501@gmail.com>
2020-08-10 07:35:39 -0300

    loginUser

--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import { key } from '../auth.json'
 import db from '../database/connection'
 
 export default {
@@ -14,5 +15,25 @@ export default {
         })
 
         return res.send('Usuário criado com sucesso!')
+    },
+    async loginUser(req: Request, res: Response) {
+        const { email, password } = req.body
+        const user = await db('users').where({
+            email
+        })
+
+        if(!user[0]) {
+            return res.status(400).send('Usuário ou senha incorretos')
+        }
+
+        if(!await bcrypt.compare(password, user[0].password)) {
+            return res.status(401).send('Usuário ou senha incorretos')
+        }
+
+        delete user[0].password
+
+        const token = jwt.sign(user[0], key, { expiresIn: '86400' })
+
+        return res.status(200).json({ user: user[0], token })
     }
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,5 +11,6 @@ routes.get('/classes', ClassesController.index)
 routes.post('/connections', ConnectionsController.create)
 routes.get('/connections', ConnectionsController.index)
 routes.post('/users', UsersController.createUser)
+routes.post('/users/login', UsersController.loginUser)
 
 export default routes
